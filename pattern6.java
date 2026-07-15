c31f88de2006281f289605fb78afd4b2c68942c7
NANDIPATI SONY <24wh1a05f1@bvrithyderabad.edu.in>
2026-07-15 13:51:57 +0530

    Add files via upload

---
+++ pattern6.java
@@ -0,0 +1,22 @@
+package pattern;
+
+public class pattern6 {
+
+	public static void main(String[] args) {
+		int n = 9;
+		for(int i=1;i<=n;i++) {
+			if(i != 1) {
+				for(int j=1;j<i;j++) {
+					System.out.print(" ");
+				}
+			}
+			for(int j=i;j<=n;j++) {
+				System.out.print(n+1-i+" ");
+			}
+			System.out.println();
+			
+				}
+			}
+		}
+
+	
